894ef6fe369aa925d32461dd402fe8fde4483a9a
j0weiss <j0weiss@icloud.com>
2020-07-17 21:27:40 +0200

    More bundles

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  // configureWebpack: {
-  //   plugins: [new BundleAnalyzerPlugin()],
-  //   optimization: {
-  //     splitChunks: {
-  //       minSize: 10000,
-  //       maxSize: 250000
-  //     }
-  //   }
-  // }
+  configureWebpack: {
+    // plugins: [new BundleAnalyzerPlugin()],
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  }
 };
